b4f93d8c8464dc3c822ea295cbbc4e0dc8d7b121
Wiped_Out <tah116emir@outlook.com>
2022-06-12 11:34:58 +0300

    Структура проекта по заветам Яндекса

---
+++ fastapi-solution/src/models/film.py
@@ -0,0 +1,18 @@
+import orjson
+from pydantic import BaseModel
+
+
+def orjson_dumps(v, *, default):
+    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
+    return orjson.dumps(v, default=default).decode()
+
+
+class Film(BaseModel):
+    id: str
+    title: str
+    description: str
+
+    class Config:
+        # Заменяем стандартную работу с json на более быструю
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
