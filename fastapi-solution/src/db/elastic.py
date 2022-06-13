108d29758b52b809b877cfacd0502a38db60758f
error1number404 <error1number404@gmail.com>
2022-06-13 19:39:07 +0700

    Delete fastapi-solution directory

--- fastapi-solution/src/db/elastic.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from elasticsearch import AsyncElasticsearch
-
-es: Optional[AsyncElasticsearch] = None
-
-
-# Функция понадобится при внедрении зависимостей
-async def get_elastic() -> AsyncElasticsearch:
-    return es
--- fastapi-solution/src/db/redis.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from aioredis import Redis
-
-redis: Optional[Redis] = None
-
-
-# Функция понадобится при внедрении зависимостей
-async def get_redis() -> Redis:
-    return redis
