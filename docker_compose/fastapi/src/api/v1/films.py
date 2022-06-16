eb06e8a94e35494a6a63d3c57c8c43dfc783cca1
Wiped_Out <tah116emir@outlook.com>
2022-06-17 00:15:27 +0300

    Сервис для получения фильмов без теста и функционала Elastic

--- docker_compose/fastapi/src/api/v1/films.py
+++ docker_compose/fastapi/src/api/v1/films.py
@@ -1,23 +1,74 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from pydantic import BaseModel
+from pydantic.types import UUID4
 
-from services.films import FilmService, get_film_service
+from services.films import (
+    FilmService, get_film_service, get_films_service, FilmsService
+)
+from typing import Optional
+from models import genre, person
 
 router = APIRouter()
 
 
-class Film(BaseModel):
-    id: str
+class FilmPage(BaseModel):
     title: str
+    imdb_rating: Optional[float] = 0.01
+    description: str
+    genres: Optional[list[genre.Genre]]
+    actors: Optional[list[person.Person]]
+    screenwriters: Optional[list[person.Person]]
+    director: Optional[person.Person]
 
 
-# Внедряем FilmService с помощью Depends(get_film_service)
-@router.get('/{film_id}', response_model=Film)
-async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
+class FilmMainPage(BaseModel):
+    id: UUID4
+    title: str
+    imdb_rating: Optional[float] = 0.01
+
+
+@router.get('/films/{film_id}', response_model=FilmPage)
+async def film_details(
+        film_id: str, film_service: FilmService = Depends(get_film_service)
+) -> FilmPage:
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
 
-    return Film(id=film.id, title=film.title)
+    return FilmPage(
+        title=film.title, description=film.description, genres=film.genres,
+        actors=film.actors, screenwriters=film.screenwriters,
+        director=film.director
+    )
+
+
+@router.get('/films')
+async def get_films_for_main_page(
+        films_service: FilmsService = Depends(get_films_service),
+        sort: Optional[str] = None,
+        genre_id: Optional[str] = Query(default=None, alias="filter[genre]")
+):
+    if sort:
+        films = await films_service.get_films(sort_param=sort)
+        if not films:
+            raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
+
+        return [FilmMainPage(**film.dict()) for film in films]
+
+    # todo
+    if genre_id:
+        return genre_id
+        pass
+
+
+@router.get("/films/search")
+async def search_for_films(
+        query: str, films_service: FilmsService = Depends(get_films_service),
+) -> list[FilmMainPage]:
+    films = await films_service.get_films(search=query)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
+
+    return [FilmMainPage(**film.dict()) for film in films]
--- docker_compose/fastapi/src/utils/utils.py
+++ docker_compose/fastapi/src/utils/utils.py
@@ -1,6 +1,7 @@
 import orjson
-
+from models.film import Film
 
 def orjson_dumps(v, *, default):
     # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
     return orjson.dumps(v, default=default).decode()
+
