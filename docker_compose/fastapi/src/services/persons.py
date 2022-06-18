7f1d0aebe1e331ae4918a2da56da7df871efdea3
Wiped_Out <tah116emir@outlook.com>
2022-06-18 17:02:02 +0300

    Получаем по айди несколько персон

--- docker_compose/fastapi/src/services/persons.py
+++ docker_compose/fastapi/src/services/persons.py
@@ -1,5 +1,4 @@
 from functools import lru_cache
-from typing import Optional
 
 from aioredis import Redis
 from elasticsearch import AsyncElasticsearch
@@ -12,52 +11,31 @@ from fuzzywuzzy import fuzz
 from services.base import BasePersonService
 
 
-class PersonService(BasePersonService):
-    async def get_person(self, person_id: str) -> Optional[Person]:
-        person = await self._get_person_from_cache(person_id=person_id)
-        if not person:
-            person = await self._get_person_from_elastic(person_id=person_id)
-            if person:
-                await self._put_person_to_cache(person=person)
-        return person
-
-    async def _get_person_from_cache(
-            self, person_id: str
-    ) -> Optional[Person]:
-        data = await self.redis.get(key=person_id)
-        if not data:
-            return None
-        return Person(**data)
-
-
 class PersonsService(BasePersonService):
-    async def get_persons(
-            self, search_param: Optional[str] = None,
-    ) -> list[Person]:
-        persons = await self._get_persons_from_cache(search_param=search_param)
+    async def search_persons(self, search: str) -> list[Person]:
+        persons = await self._get_persons_from_cache_by_search(search=search)
         if not persons:
-            persons = await self._get_persons_from_elastic(search_param=search_param)
+            persons = await self._search_persons_in_elastic(search=search)
             if persons:
                 await self._put_persons_to_cache(persons=persons)
         return persons
 
-    async def _get_persons_from_elastic(
-            self, search_param: Optional[str] = None
-    ) -> list[Person]:
-        if search_param:
-            return await self._search_persons_in_elastic(search=search_param)
-        return await self._get_all_persons_from_elastic()
+    async def get_persons_by_id(self, person_id: str) -> list[Person]:
+        persons = await self._get_person_from_cache(person_id=person_id)
+        if not persons:
+            persons = await self._get_person_from_elastic(person_id=person_id)
+            if persons:
+                await self._put_persons_to_cache(persons=persons)
+        return persons
 
-    async def _get_persons_from_cache(
-            self, search_param: Optional[str] = None,
-    ) -> list[Person]:
+    async def _get_persons_from_cache_by_search(self, search: str) -> list[Person]:
         data = []
 
-        if search_param:
+        if search:
             for key in self.redis.keys("*"):
                 person = Person(**self.redis.get(key=key))
                 if fuzz.WRatio(
-                        search_param.lower(), person.full_name.lower()
+                        search.lower(), person.full_name.lower()
                 ) > 80:
                     data.append(person)
 
@@ -68,17 +46,15 @@ class PersonsService(BasePersonService):
 
         return data
 
+    async def _get_person_from_cache(self, person_id: str) -> list[Person]:
+        data = await self.redis.get(key=person_id)
+        if not data:
+            return []
+        return [Person.parse_raw(person) for person in data]
+
     async def _put_persons_to_cache(self, persons: list[Person]):
         for person in persons:
             await self._put_person_to_cache(person=person)
-
-
-@lru_cache()
-def get_person_service(
-        redis: Redis = Depends(get_redis),
-        elastic: AsyncElasticsearch = Depends(get_elastic),
-) -> PersonService:
-    return PersonService(redis=redis, elastic=elastic)
 
 
 @lru_cache()
