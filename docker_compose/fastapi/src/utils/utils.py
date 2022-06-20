69cd149f73f96b23f409f0986f76a3086eec26bf
Остап <error1number404@gmail.com>
2022-06-21 04:08:48 +0700

    Функция для пагинации + чистый requirements.txt

--- docker_compose/fastapi/src/utils/utils.py
+++ docker_compose/fastapi/src/utils/utils.py
@@ -1,7 +1,18 @@
+from typing import TypeVar, Sequence
+
 import orjson
+from fastapi_pagination import Params, Page
+from pydantic import conint
+
+T = TypeVar("T")
 
 
 def orjson_dumps(v, *, default):
     # orjson.dumps возвращает bytes,
     # а pydantic требует unicode, поэтому декодируем
     return orjson.dumps(v, default=default).decode()
+
+
+def paginate(items: Sequence[T], total: conint(ge=1), page: conint(ge=1), size: conint(ge=1)):
+    params = Params(page=page, size=size)
+    return Page.create(items=items, total=total, params=params)
