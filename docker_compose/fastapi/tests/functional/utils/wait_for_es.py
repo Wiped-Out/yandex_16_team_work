0e9279743f066f90c60aa8101052ca9b8e39cf99
Остап <error1number404@gmail.com>
2022-07-04 01:58:38 +0700

    Backoff wait_for

--- docker_compose/fastapi/tests/functional/utils/wait_for_es.py
+++ docker_compose/fastapi/tests/functional/utils/wait_for_es.py
@@ -1,12 +1,27 @@
-from settings import settings
 from elasticsearch import AsyncElasticsearch
-from utils.utils import backoff
+
+from settings import settings
+from .utils import backoff_on_true, backoff
 
 
-@backoff()
+class ElasticManager:
+    """Контекстный менеджер для работы с Elasticsearch"""
+
+    @backoff()
+    async def connect(self):
+        self.connection = AsyncElasticsearch(
+            hosts=[f'{settings.ELASTIC_HOST}:{settings.ELASTIC_PORT}']
+        )
+
+    async def __aenter__(self) -> AsyncElasticsearch:
+        await self.connect()
+        return self.connection
+
+    async def __aexit__(self, error: Exception, value: object, traceback: object):
+        await self.connection.close()
+
+
+@backoff_on_true()
 async def wait_for_es():
-    es = AsyncElasticsearch(
-        hosts=[f'{settings.ELASTIC_HOST}:{settings.ELASTIC_PORT}']
-    )
-
-    await es.close()
+    async with ElasticManager() as es:
+        return await es.ping()
--- docker_compose/fastapi/tests/functional/utils/wait_for_redis.py
+++ docker_compose/fastapi/tests/functional/utils/wait_for_redis.py
@@ -1,13 +1,29 @@
 import aioredis
+from aioredis import Redis
 
 from settings import settings
-from utils.utils import backoff
+from .utils import backoff_on_true, backoff
 
 
-@backoff
+class RedisManager:
+    """Контекстный менеджер для работы с Redis"""
+
+    @backoff()
+    async def connect(self):
+        self.connection = await aioredis.create_redis_pool((
+            settings.REDIS_HOST, settings.REDIS_PORT
+        ), minsize=10, maxsize=20)
+
+    async def __aenter__(self) -> Redis:
+        await self.connect()
+        return self.connection
+
+    async def __aexit__(self, error: Exception, value: object, traceback: object):
+        self.connection.close()
+        await self.connection.wait_closed()
+
+
+@backoff_on_true()
 async def wait_for_redis():
-    redis = await aioredis.create_redis_pool((
-        settings.REDIS_HOST, settings.REDIS_PORT
-    ), minsize=10, maxsize=20)
-    redis.close()
-    await redis.wait_closed()
+    async with RedisManager() as redis:
+        return await redis.ping()
