30b3c715c02f80e77a52c5c1565b044ca48ae725
Остап <error1number404@gmail.com>
2022-08-02 13:45:04 +0700

    auth service model param

--- docker_compose/fastapi/src/services/auth_integration.py
+++ docker_compose/fastapi/src/services/auth_integration.py
@@ -12,10 +12,11 @@ from utils.utils import decode_jwt
 
 
 class AuthService(BaseCacheStorage, BaseRequest):
+    model = AuthUser
 
     async def auth_user(self, Authorization: str):
         try:
-            user = await self.get_one_item_from_cache(cache_key=Authorization, model=AuthUser)
+            user = await self.get_one_item_from_cache(cache_key=Authorization, model=self.model)
             if user:
                 return user
             exp_time = 300
@@ -24,9 +25,9 @@ class AuthService(BaseCacheStorage, BaseRequest):
                 response = await self.get(
                     url=f"{settings.AUTH_SERVICE_URL}/users/{decoded_jwt['sub']}/role/highest_role",
                     headers={"Authorization": f"Bearer {Authorization}"})
-                user = AuthUser(highest_role=response.body['level'], uuid=decoded_jwt['sub'])
+                user = self.model(highest_role=response.body['level'], uuid=decoded_jwt['sub'])
             except ConnectionError:
-                user = AuthUser(highest_role=0, uuid=decoded_jwt['sub'])
+                user = self.model(highest_role=0, uuid=decoded_jwt['sub'])
                 exp_time = 60
             await self.put_one_item_to_cache(cache_key=Authorization, item=user, expire=exp_time)
             return user
