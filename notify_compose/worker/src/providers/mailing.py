341d093a984e35aeb0bdcebf9e39986e8cec3b8b
Emir Takhaviev <58186925+Wiped-Out@users.noreply.github.com>
2022-09-24 21:45:46 +0300

    delete build functions

--- notify_compose/worker/src/providers/mailing.py
+++ notify_compose/worker/src/providers/mailing.py
@@ -7,6 +7,6 @@ mailing_client: Optional[AbstractMailingClient] = None
 
 # Функция понадобится при внедрении зависимостей
 async def get_mailing_client() -> AbstractMailingClient:
-    if not mailing_client:
+    if mailing_client is None:
         raise
     return mailing_client
--- notify_compose/worker/src/services/mailing.py
+++ notify_compose/worker/src/services/mailing.py
@@ -1,29 +1,7 @@
-from models.models import EmailSender, EmailReceiver, EmailMessage
+from models.models import EmailMessage
 from services.mailing_client import BaseMailingClient
 
 
 class MailingService(BaseMailingClient):
     async def send_email(self, messages: list[EmailMessage]):
         await self.send(messages=messages)
-
-    async def build_message(
-            self,
-            sender: EmailSender,
-            receivers: list[EmailReceiver],
-            subject: str,
-            text: str,
-            html: str,
-    ) -> EmailMessage:
-        return EmailMessage(
-            From=sender,
-            To=receivers,
-            Subject=subject,
-            TextPart=text,
-            HTMLPart=html,
-        )
-
-    async def build_sender(self, sender_email: str, sender_name: str) -> EmailSender:
-        return EmailSender(Email=sender_email, Name=sender_name)
-
-    async def build_receiver(self, receiver_email: str, receiver_name: str) -> EmailReceiver:
-        return EmailReceiver(Email=receiver_email, Name=receiver_name)
