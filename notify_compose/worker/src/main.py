a940e0655e3115b77307b01703b7347b420a0d58
askalach <askalach@gmail.com>
2022-09-26 00:55:33 +0300

    add filling for mailing service

--- notify_compose/worker/src/main.py
+++ notify_compose/worker/src/main.py
@@ -9,9 +9,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from core.config import settings
 from db import db
-from models.models import Notification, NotificationTypeEnum, Template
+from models.models import (EmailMessage, EmailReceiver, EmailSender,
+                           Notification, NotificationTypeEnum, Template,
+                           TemplateTypeEnum)
 from providers import mailing
 from services.data_scrapper import AsyncScrapper
+from services.mailing import MailingService
 from services.mailing_client import MailJetMailingClient
 from services.main_db import BaseMongoStorage
 from services.templater import Templater
@@ -30,6 +33,35 @@ async def on_message(message: AbstractIncomingMessage) -> None:
         scrapper = AsyncScrapper(items=template.fields, ready_data={"user_id": data['user_id']})
         ready_data = scrapper.get_result()
         ready_template = Templater.render(template, ready_data)
+
+        scrapper = AsyncScrapper(
+            items={
+                'name': 'user',
+                'url': 'http://nginx_auth:80/api/users/,start,user_id,end,',
+                'body': {},
+                'headers': {},
+                'fetch_result': ""
+            },
+            ready_data={"user_id": data['user_id']}
+        )
+        ready_user_data = scrapper.get_result()
+
+        mailer = MailingService()
+        mailer.send_email(messages=[
+            EmailMessage(
+                From=EmailSender(
+                    Email=settings.SENDER_EMAIL,
+                    Name=settings.SENDER_NAME
+                ),
+                To=EmailReceiver(
+                    Email=ready_user_data['email'],
+                    Name=ready_user_data['login']
+                ),
+                Subject=template.subject,
+                TextPart=ready_template if template.template_type is TemplateTypeEnum.plain else "",
+                HTMLPart=ready_template if template.template_type is TemplateTypeEnum.html else ""
+            )
+        ])
 
 
 async def main() -> None:
